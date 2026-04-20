50de3732858e7588ca625c52b5a86b39ba933a31
Manjiri <manjirikshirsagar98@gmail.com>
2026-04-20 21:24:22 +0530

    3rd commit by manjiri

--- GitJenkinsProj/src/com/Test.java
+++ GitJenkinsProj/src/com/Test.java
@@ -5,6 +5,6 @@ public class Test
 	public static void main(String[] args) {
 		
 		System.out.println("this is the main method");
-		System.out.println("this is the main method2");
+		System.out.println("this is the main method3");
 	}
 }
