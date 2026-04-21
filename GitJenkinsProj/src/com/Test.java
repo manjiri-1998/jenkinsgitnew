daaf0c2f103e4a559085b7bdf177554c40fb1817
Manjiri <manjirikshirsagar98@gmail.com>
2026-04-21 16:41:20 +0530

    new method added by manjiri

--- GitJenkinsProj/src/com/Test.java
+++ GitJenkinsProj/src/com/Test.java
@@ -3,8 +3,14 @@ package com;
 public class Test 
 {
 	public static void main(String[] args) {
-		
+		Test t = new Test();
+		t.m1();
 		System.out.println("this is the main method");
 		System.out.println("this is the main method3");
+	}
+	
+	public void m1()
+	{
+		System.out.println("hello m1 method");
 	}
 }
